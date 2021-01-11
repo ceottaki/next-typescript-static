89b4fe0505bb4ff3d43c8a1e16c4333a4916c137
Felipe Ceotto <felipe.ceotto@gamingmechanics.com>
2021-01-11 19:18:21 +0000

    Simple support for lazy loading CSS background images.

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,8 +1,22 @@
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import '../global-styles/global-styles.scss'
 import withLazyLoading from '../components/Utilities/withLazyLoading'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Adds simple support for background images:
+    document.addEventListener('lazybeforeunveil', (e: Event) => {
+      if (e && e.target) {
+        const element = e.target as HTMLElement
+        const bg = element.getAttribute('data-bg')
+        if (bg) {
+          element.style.backgroundImage = 'url(' + bg + ')'
+        }
+      }
+    })
+  }, [])
+
   return <Component {...pageProps} />
 }
 
